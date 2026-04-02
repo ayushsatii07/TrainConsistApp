24ef23f898d12fc8cb21360880e2fde2c0ad8e53
ayushsatii07 <ayushsati844@gmail.com>
2026-04-02 10:13:22 +0530

    UC6

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
-import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
-        System.out.println("UC5: Ordered Unique Bogie Formation\n");
+        System.out.println("UC6: Bogie Capacity Mapping\n");
 
-        LinkedHashSet<String> train = new LinkedHashSet<>();
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
 
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("Cargo");
-        train.add("Guard");
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 60);
+        bogieCapacity.put("First Class", 24);
 
-        train.add("Sleeper");
-
-        System.out.println("Final Train Formation:");
-        System.out.println(train);
+        System.out.println("Bogie Capacity Details:");
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
