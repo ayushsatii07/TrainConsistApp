a7ddcd5eee4bb9a6272393a1da12f76960708c64
AYUSH SATI <ayushsati844@gmail.com>
2026-04-02 09:56:53 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class UC2_TrainApp {
+public class Main {
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
